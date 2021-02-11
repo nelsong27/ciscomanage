0f98a609e7c5159189151d3354aae0040b38a018
Jerry Mozes <jerry@puppet.com>
2021-02-11 16:52:12 -0700

    change peserver to string value

--- plans/config.pp
+++ plans/config.pp
@@ -2,10 +2,10 @@
 # @param tclfile The name of the file that contains the script to run at the device.
 # @param devuser The user on the device to use to authenticate with.
 # @param type The type of device that is being connect to.  Either ios or nxos are acceptable.
-# @param peserver The Puppet Server to run the plan with
+# @param peserver The Puppet Server to run the plan with.
 plan ciscomanage::config (
 Targetspec $targets,
-Targetspec $peserver,
+String $peserver,
 String $tclfile,
 String $devuser,
 Enum['ios', 'nxos'] $type

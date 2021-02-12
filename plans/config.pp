b40ff3a21de561984f4d5ac2d1e7ca084990c29d
Jerry Mozes <jerry@puppet.com>
2021-02-12 12:38:44 -0700

    change description of upload script

--- plans/config.pp
+++ plans/config.pp
@@ -18,7 +18,7 @@ Enum['ios', 'nxos'] $type
     case $type {
       'ios': {
         # copy up the tcl file using scp.   Uses sshkey
-        run_command("scp -i ~/.ssh/id_rsa /etc/puppetlabs/code/environments/production/modules/ciscomanage/files/${tclfile} ${devuser}@${target}:flash:${tclfile}", $peserver, 'Uploading script to device')
+        run_command("scp -i ~/.ssh/id_rsa /etc/puppetlabs/code/environments/production/modules/ciscomanage/files/${tclfile} ${devuser}@${target}:flash:${tclfile}", $peserver, "Uploading script to ${target}")
         ctrl::sleep(5)
         # run command on the target to run the script that was uploaded
         run_command("tclsh ${tclfile}", $target, "Configure the ${target}")
@@ -28,7 +28,7 @@ Enum['ios', 'nxos'] $type
       }
       'nxos': {
         # copy up the tcl file using scp.   Uses sshkey
-        run_command("scp -i ~/.ssh/id_rsa /etc/puppetlabs/code/environments/production/modules/ciscomanage/files/${tclfile} ${devuser}@${target}:bootflash:${tclfile}", $peserver, 'Uploading script to device')
+        run_command("scp -i ~/.ssh/id_rsa /etc/puppetlabs/code/environments/production/modules/ciscomanage/files/${tclfile} ${devuser}@${target}:bootflash:${tclfile}", $peserver, "Uploading script to ${target}")
         ctrl::sleep(5)
         # run command on the target to run the script that was uploaded
         run_command("tclsh bootflash:${tclfile}", $target, "Configure device ${target}")

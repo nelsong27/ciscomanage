3e71acc5640dceaffca28310a3b109f889c0e4e9
Jerry Mozes <jerry@puppet.com>
2021-02-23 13:45:48 -0700

    comment out copy run start

--- files/nxosportcfg.tcl
+++ files/nxosportcfg.tcl
@@ -5,8 +5,4 @@ cli interface Ethernet1/2
 cli switchport mode access
 cli switchport access vlan 200
 cli no shutdown
-<<<<<<< HEAD
 # cli copy running-config startup-config
-=======
-cli copy running-config startup-config
->>>>>>> f119f64f3fd205c107c74591d255c5d085fcadd5
